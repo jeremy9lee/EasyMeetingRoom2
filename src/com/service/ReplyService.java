786b7267a05736dce00148a04e638878c0fd0fc5
student <student@M140416>
2015-05-28 10:06:47 +0900

    ERROR SOLVED

--- src/com/service/ReplyService.java
+++ src/com/service/ReplyService.java
@@ -13,15 +13,15 @@ public class ReplyService {
 	@Autowired
 	private ReplyDao dao;
 	
-	public List<Reply> fetchList(){
-		return dao.fetchList();
+	public List<Reply> fetchList(String roomNo, String buildingNo){
+		return dao.fetchList(roomNo, buildingNo);
 	}
 	
 	public void insertContent(Reply vo){
 		dao.insert(vo);
 	}
 
-	public void deleteContent(Reply vo){
-		dao.delete(vo);
+	public void deleteContent(String replyNo){
+		dao.delete(replyNo);
 	}
 }
